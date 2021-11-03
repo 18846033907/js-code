afe34f05f4c5e02b75f6151f8a5aad58297f46a6
wenkang.yang <wenkang.yang@deltaww.com>
2021-11-03 18:03:05 +0800

    test

--- code.js
+++ code.js
@@ -58,13 +58,57 @@ function typeOf(obj) {
 // console.log(flatten(aR));
 
 //深浅拷贝
+//浅拷贝
 function shallowCopy(obj) {
-  if (!typeOf(obj)) return;
-  const newObj = {};
+  if (typeOf(obj) !== "object") return;
+  var newObj = obj instanceof Array ? [] : {};
   for (let prop in obj) {
     if (obj.hasOwnProperty(prop)) {
-      newObj.prop = newObj[prop];
+      newObj[prop] = obj[prop];
     }
   }
   return newObj;
 }
+//深拷贝
+function deepCopy(obj) {
+  if (typeOf(obj) !== ("object" || "array")) return obj;
+  const newObj = obj instanceof Array ? [] : {};
+  for (let prop in obj) {
+    console.log(obj[prop]);
+    if (obj.hasOwnProperty(prop)) {
+      newObj[prop] = deepCopy(obj[prop]);
+    }
+  }
+  return newObj;
+}
+
+// function deepCopy(obj) {
+//   if (obj === null) return obj;
+//   if (obj instanceof Date) return new Date(obj);
+//   if (obj instanceof RegExp) return new RegExp(obj);
+//   if (typeof obj !== "object") return obj;
+//   let cloneObj = new obj.constructor();
+//   for (let key in obj) {
+//     if (obj.hasOwnProperty(key)) {
+//       // 实现一个递归拷贝
+//       cloneObj[key] = deepCopy(obj[key]);
+//     }
+//   }
+//   return cloneObj;
+// }
+
+const obj = {
+  name: "浪里行舟",
+  arr: [1, [2, 3], 4],
+};
+
+// console.log(shallowCopy(obj), deepCopy(obj));
+// const obj1 = shallowCopy(obj);
+// const obj2 = shallowCopy(obj);
+// obj1.arr[1] = [5,6,7]
+// console.log(obj1, obj);
+
+const obj1 = deepCopy(obj);
+// const obj2 = deepCopy(obj);
+// obj1.arr[1] = [5, 6, 7];
+// console.log(obj1, obj);
