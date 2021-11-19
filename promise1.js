7f631a8807f0e6d9850f35b28536fb876d7897e2
wenkang.yang <wenkang.yang@deltaww.com>
2021-11-19 17:37:41 +0800

    feat:test

--- promise1.js
+++ promise1.js
@@ -318,7 +318,7 @@ function pubFront(strs) {
     throw new TypeError("this is not an array");
   let str = strs.length > 0 ? strs[0] : "";
   for (let key in strs) {
-    if (str === "") break
+    if (str === "") break;
     const item = strs[key];
     while (!item.includes(str)) {
       if (str === "") break;
@@ -332,8 +332,8 @@ function pubFront(strs) {
 
 const jsonp = ({ url, params, callbackName }) => {
   const generateURL = () => {
-    let dataStr = '';
-    for(let key in params) {
+    let dataStr = "";
+    for (let key in params) {
       dataStr += `${key}=${params[key]}&`;
     }
     dataStr += `callback=${callbackName}`;
@@ -341,24 +341,26 @@ const jsonp = ({ url, params, callbackName }) => {
   };
   return new Promise((resolve, reject) => {
     // 初始化回调函数名称
-    callbackName = callbackName 
+    callbackName = callbackName;
     // 创建 script 元素并加入到当前文档中
-    let scriptEle = document.createElement('script');
+    let scriptEle = document.createElement("script");
     scriptEle.src = generateURL();
     document.body.appendChild(scriptEle);
-    window[callbackName] = (data) => {console.log(55,data)}
+    window[callbackName] = (data) => {
+      console.log(55, data);
+    };
     // 绑定到 window 上，为了后面调用
     window[callbackName] = (data) => {
       resolve(data);
       // script 执行完了，成为无用元素，需要清除
       document.body.removeChild(scriptEle);
-    }
+    };
   });
-}
+};
 
 // jsonp({
 //   url: 'http://localhost:3000',
-//   params: { 
+//   params: {
 //     a: 1,
 //     b: 2
 //   },
@@ -369,6 +371,174 @@ const jsonp = ({ url, params, callbackName }) => {
 //   console.log(data); // 数据包
 // })
 
+function compose(...args) {
+  let result;
+  return function (x) {
+    result = x;
+    while (args.length > 0) {
+      result = args.pop()(result);
+    }
+    return result;
+  };
+}
 
+// // 用法如下:
+// function fn1(x) {
+//   return x + 1;
+// }
+// function fn2(x) {
+//   return x + 2;
+// }
+// function fn3(x) {
+//   return x + 3;
+// }
+// function fn4(x) {
+//   return x + 4;
+// }
+// const a = compose(fn1, fn2, fn3, fn4);
+// console.log(a(1)); // 1+4+3+2+1=11
 
+// //组合继承
+// function Parent(name, age) {
+//   this.name = name;
+//   this.age = {
+//     jj: 4,
+//     age,
+//   };
+// }
+// Parent.prototype.sayname = function () {
+//   console.log(this.name);
+// };
+// Parent.prototype.sayage = function () {
+//   console.log(this.age);
+// };
 
+// function Child() {
+//   Parent.apply(this, arguments);
+//   this.sex = "女";
+// }
+
+// function generateObj(obj) {
+//   let F = function () {};
+//   F.prototype = obj.prototype;
+//   return new F();
+// }
+
+// function inherit(Child, Parent) {
+//   const prototype = generateObj(Parent);
+//   // Child.prototype.__proto__=Parent.prototype
+//   prototype.constructor = Child;
+//   Child.prototype = prototype;
+//   return Child;
+// }
+// const child = inherit(Child, Parent);
+// // Child.prototype = Object.create(Parent.prototype);
+// // Child.prototype.constructor = Child;
+
+// const child1 = new child("kkk", 9);
+// const child2 = new child("jjj", 3);
+// child1.age.jj = 8;
+// child1.sayage();
+// child2.sayage();
+
+//数组去重
+// function uniqArr(arr) {
+//   const newArr=[]
+//   for (let key in arr) {
+//     const item = arr[key];
+//     if (arr.indexOf(item) === Number(key)) {
+//      newArr.push(item)
+//     }
+//   }
+//   return newArr
+// }
+
+// function uniqArr(arr) {
+//   const newArr=[]
+//   for (let key in arr) {
+//     const item = arr[key];
+//     if (!newArr.includes(item)) {
+//      newArr.push(item)
+//     }
+//   }
+//   return newArr
+// }
+
+// const arr=[1,2,3,1,2,3,4,5,2,3,4]
+// console.log(uniqArr(arr))
+
+// //数组扁平化
+// function flatten(arr) {
+//   //校验是否维数组
+//   let result = arr.reduce(function (pre, cur) {
+//     return Array.isArray(cur) ? [...pre, ...flatten(cur)] : [...pre, cur];
+//   }, []);
+//   return result;
+// }
+
+// function flatten(arr) {
+//   while (
+//     arr.some((item) => {
+//       return Array.isArray(item);
+//     })
+//   ) {
+//     arr = [].concat(...arr);
+//   }
+//   return arr;
+// }
+
+// const arr = [2, 4, [3, 2, 2, [4, 5, 9]], [0, 4, 5, 6]];
+// console.log(flatten(arr));
+
+//浅拷贝
+// function shallowCopy(obj) {
+//   const newObj = {};
+//   for (let key in obj) {
+//     if (obj.hasOwnProperty(key)) {
+//       newObj[key] = obj[key];
+//     }
+//   }
+//   return newObj;
+// }
+// const obj = {
+//   name: "lihan",
+//   age: 40,
+//   sex: {
+//     one: "nv",
+//     two: "nan",
+//   },
+// };
+// const obj1 = shallowCopy(obj);
+// obj1.name = "ooo";
+// const obj2 = shallowCopy(obj);
+// obj2.sex.one = "ppp";
+// console.log(obj1, obj2);
+
+//深拷贝
+function deepCopy(obj) {
+  const newObj = {};
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      if (typeof obj[key] === "object") {
+        newObj[key] = deepCopy(obj[key]);
+      } else {
+        newObj[key] = obj[key];
+      }
+    }
+  }
+  return newObj
+}
+
+const obj = {
+  name: "lihan",
+  age: 40,
+  sex: {
+    one: "nv",
+    two: "nan",
+  },
+};
+const obj1 = deepCopy(obj);
+obj1.name = "ooo";
+const obj2 = deepCopy(obj);
+obj2.sex.one = "ppp";
+console.log(obj1, obj2);
