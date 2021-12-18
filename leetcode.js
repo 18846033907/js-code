61b539f8ba265225ec96086f68a5420c4ae34290
lihan <ywk0422@163.com>
2021-12-19 00:08:36 +0800

    test

--- leetcode.js
+++ leetcode.js
@@ -1,248 +1,295 @@
-//斐波那契[1,1,2,3,5,8,13,21...]
-function fibonacci(n) {
-  if (n <= 1) return 1;
-  const arr = [1, 1];
-  let i = n + 1 - 2;
-  while (i > 0) {
-    const a = arr[arr.length - 2],
-      b = arr[arr.length - 1];
-    arr.push(a + b);
-    i--;
-  }
-  return arr[arr.length - 1];
-}
-// console.log(fibonacci(5));
-
-//输入一个正数N，输出所有和为N的连续正数序列，例如输入15，输出[[1,2,3,4,5],[4,5,6],[7,8]]
-
-//两数之和
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
-const twoSum = function (nums, target) {
-  let arr = [];
-  let index = 0;
-  const len = nums.length;
-  while (index < len) {
-    const item = nums[index];
-    const temp = target - item;
-    if (nums.includes(temp) && nums.indexOf(temp) !== index) {
-      const i = nums.indexOf(temp);
-      arr = [index, i];
-      return arr;
-    }
-    index++;
-  }
-};
-
-// const twoSum = function (nums, target) {
-//   const len = nums.length;
-//   for (let i = 0; i < len - 1; i++) {
-//     for (let j = i + 1; j < len; j++) {
-//       if (nums[i] + nums[j] === target) {
-//         return [i, j];
-//       }
-//     }
-//   }
-// };
-// const nums = [3, 2, 4],
-//   target = 6;
-// console.log(twoSum(nums, target));
-//两数之和
-// function twoSum(nums, target) {
-//   let temp = {};
-//   const len = nums.length;
-//   for (i = 0; i < len; i++) {
-//     const current = nums[i];
-//     if (temp[target - current] !== undefined) {
-//       return [temp[target - current], i];
-//     }
-//     temp[current] = i;
-//   }
+//斐波那契数列
+//暴力递归
+// function fib(n) {
+//   if (n === 0) return 0;
+//   if (n === 1) return 1;
+//   return fib(n - 1) + fib(n - 2);
 // }
-// console.log(twoSum([1, 2, 3, 4, 3, 13], 15));
+//递归+记忆化
+function fib(n) {
+  const memo = {};
+  const helper = function (m) {
+    if (memo[m]) return memo[m];
+    if (m === 0) return 0;
+    if (m === 1) return 1;
+    memo[m] = helper(m - 1) + helper(m - 2);
+    return memo[m];
+  };
+  return helper(n);
+}
+//动态规划
+function fib(n) {
+  if (n <= 1) return n;
+  let memo = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    memo[i] = memo[i - 1] + memo[i - 2];
+  }
+  return memo[n];
+}
+//动态数组
+function fib(n) {
+  if (n <= 1) return n;
+  let memo = [0, 1];
+  let sum = null;
+  for (let i = 2; i <= n; i++) {
+    sum = memo[0] + memo[1];
+    memo[0] = memo[1];
+    memo[1] = sum;
+  }
+  return sum;
+}
+// console.log(fib(5));s
 
-//三数之和
-// const nums = [-1, 0, 1, 2, -1, -4];
-
-// [
-//   [-1, 0, 1],
-//   [-1, -1, 2]
-// ]
-
-function threeSum(nums, target) {
-  let result = [];
-  const newArr = nums.sort(function (a, b) {
-    return a - b;
+//不同路径
+function uniqPath(m, n) {
+  const memo = new Array(m).fill(0).map(() => {
+    return new Array(n).fill(0);
   });
-  const len = newArr.length;
-  for (i = 0; i < len; i++) {
-    const item = newArr[i];
-    let l = 1,
-      r = 1;
-    function restFn() {
-      const rI = len - r,
-        lI = i + l;
-      const rest = newArr[rI] + newArr[lI];
-      if (rest + item === target && i !== rI && i !== lI && rI !== lI) {
-        result.push([newArr[i], newArr[i + l], newArr[len - r]]);
-      } else if (rest + item > target) {
-        r++;
-        restFn();
-      } else if (rest + item < target) {
-        l++;
-        restFn();
+  for (let i = 0; i < m; i++) {
+    memo[i][0] = 1;
+  }
+  for (let j = 0; j < n; j++) {
+    memo[0][j] = 1;
+  }
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      memo[i][j] = memo[i - 1][j] + memo[i][j - 1];
+    }
+  }
+  return memo[m - 1][n - 1];
+}
+
+//状态压缩
+function uniqPath(m, n) {
+  const memo = new Array(n).fill(1);
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      memo[j] = memo[j - 1] + memo[j];
+    }
+  }
+  return memo[n - 1];
+}
+function uniqPathWithObstacles(obstaArr) {
+  const m = obstaArr.length;
+  const n = obstaArr[0].length;
+  const memo = new Array(m).fill(0).map(() => {
+    return new Array(n).fill(0);
+  });
+  for (let i = 0; i < m; i++) {
+    if (obstaArr[i][0] === 0) {
+      memo[i][0] = 1;
+    }
+  }
+  for (let j = 0; j < n; j++) {
+    if (obstaArr[0][j] === 0) {
+      memo[0][j] = 1;
+    }
+  }
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      memo[i][j] = obstaArr[i][j] === 1 ? 0 : memo[i - 1][j] + memo[i][j - 1];
+    }
+  }
+  console.log(memo);
+  return memo[m - 1][n - 1];
+}
+
+//状态压缩
+function uniqPathWithObstacles(obstracArr) {
+  const m = obstracArr.length;
+  const n = obstracArr[0].length;
+  let memo = new Array(n).fill(0);
+  memo[0] = 1;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (obstracArr[i][j] === 1) {
+        memo[j] = 0;
+      } else if (j > 0) {
+        memo[j] = memo[j - 1] + memo[j];
       }
     }
-    restFn();
   }
-  return result;
+  console.log(memo);
+  return memo[n - 1];
 }
-// console.log(threeSum(nums, 0));
 
-//容器最多水
-// 输入：[1,8,6,2,5,4,8,3,7]
-// 输出：49
-function mostWater(nums) {
-  const len = nums.length;
-  let result = 0,
-    left = 0,
-    right = len - 1;
-  while (left < right) {
-    const min = Math.min(nums[left], nums[right]);
-    const area = min * (right - left);
-    result = Math.max(result, area);
-    if (nums[left] < nums[right]) {
-      left++;
-    } else {
-      right--;
-    }
-  }
-  return result;
-}
-// console.log(mostWater([1,8,6,2,5,4,8,3,7]))
+// console.log(
+//   uniqPathWithObstacles([
+//     [0, 0, 0],
+//     [0, 1, 0],
+//     [0, 0, 0],
+//   ])
+// );
+
 //爬楼梯
-function climb(n) {
-  if (n === 0 || n === 1) return 1;
-  const dp = [];
-  dp[0] = 1;
-  dp[1] = 1;
-  for (i = 2; i <= n; i++) {
-    dp[i] = dp[i - 1] + dp[i - 2];
+function climbstair(n) {
+  if (n <= 1) return n;
+  const memo = [1, 1];
+  for (let i = 2; i <= n; i++) {
+    memo[i] = memo[i - 1] + memo[i - 2];
   }
-  return dp[n];
+  console.log(memo);
+  return memo[n];
 }
-// console.log(climb(6))
+//状态压缩
+function climbstair(n) {
+  if (n <= 1) return n;
+  const memo = [1, 1];
+  let sum = null;
+  for (let i = 2; i <= n; i++) {
+    sum = memo[0] + memo[1];
+    memo[0] = memo[1];
+    memo[1] = sum;
+  }
+  return sum;
+}
+// console.log(climbstair(3))
 
-//有效括号
-function valid(str) {
-  const len = str.length;
-  if (len % 2 !== 0) return false;
-  const obj = { "(": ")", "{": "}", "[": "]" };
-  let res = [];
-  for (let prop of str) {
-    if (prop in obj) {
-      res.push(prop);
-    } else {
-      if (obj[res.pop()] !== prop) {
-        return false;
-      }
+//完全平方和
+
+function completeSquare(n) {
+  let memo = [];
+  for (i = 0; i <= n; i++) {
+    memo[i] = i;
+    for (j = 1; i - j * j >= 0; j++) {
+      memo[i] = Math.min(memo[i], memo[i - j * j] + 1);
     }
   }
-  return !res.length;
+  return memo[n];
 }
-const str = "({}]{}";
-// console.log(valid(str));
-//零钱兑换
-function coinsChange(coins, mount) {
-  let memo = Array(mount + 1).fill(Infinity);
+// console.log(completeSquare(4));
+//换硬币
+function coinsChange(amount, coins) {
+  const memo = new Array(amount + 1).fill(Infinity);
   memo[0] = 0;
-  for (let i = 1; i <= mount; i++) {
+  for (let i = 0; i <= amount; i++) {
     for (let j = 0; j < coins.length; j++) {
       if (i - coins[j] >= 0) {
-        console.log(166, i, j);
         memo[i] = Math.min(memo[i], memo[i - coins[j]] + 1);
       }
     }
   }
-  return memo[mount] === Infinity ? -1 : memo[mount];
+  return memo[amount];
 }
-const coinChange = function (coins, amount) {
-  // 用于保存每个目标总额对应的最小硬币个数
-  const f = [];
-  // 提前定义已知情况
-  f[0] = 0;
-  // 遍历 [1, amount] 这个区间的硬币总额
-  for (let i = 1; i <= amount; i++) {
-    // 求的是最小值，因此我们预设为无穷大，确保它一定会被更小的数更新
-    f[i] = Infinity;
-    // 循环遍历每个可用硬币的面额
-    for (let j = 0; j < coins.length; j++) {
-      // 若硬币面额小于目标总额，则问题成立
-      if (i - coins[j] >= 0) {
-        // 状态转移方程
-        f[i] = Math.min(f[i], f[i - coins[j]] + 1);
+
+// console.log(coinsChange(10,[1,2,5]))
+//分发饼干
+function districookie(childArr, cookieArr) {
+  const k = childArr.sort((a, b) => a - b);
+  const c = cookieArr.sort((a, b) => a - b);
+  let index = c.length - 1;
+  let result = 0;
+  for (let i = k.length - 1; i >= 0; i--) {
+    if (index >= 0 && c[index] >= k[i]) {
+      result++;
+      index--;
+    }
+  }
+  return result;
+}
+// console.log(districookie([1,7,10,2],[1,3,5,9]))
+//两数之和
+function twoSum(nums, target) {
+  const m = new Map();
+  let len = nums.length;
+  let result = -1;
+  for (let i = 0; i < len; i++) {
+    const cur = nums[i];
+    if (m.has(target - cur)) {
+      result = [m.get(target - cur), i];
+    } else {
+      m.set(cur, i);
+    }
+  }
+  return result;
+}
+
+// console.log(twoSum([1, 3, 5], 8));
+
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+function addTwoNumbers(l1, l2) {
+  let dummy = new ListNode();
+  let curr = dummy;
+  let carry = 0;
+  while (l1 !== null || l2 !== null) {
+    let sum = 0;
+    if (l1 !== null) {
+      sum += l1.val;
+      l1 = l1.next;
+    }
+    if (l2 !== null) {
+      sum += l2.val;
+      l2 = l2.next;
+    }
+    sum += carry;
+    curr.next = new ListNode(sum % 10);
+    carry = Math.floor(sum / 10);
+    curr = curr.next;
+  }
+  if (carry > 0) {
+    curr.next = new ListNode(carry);
+  }
+  console.log(235, dummy);
+  return dummy.next;
+}
+// const l1 = new ListNode(2);
+// l1.next = new ListNode(4);
+// l1.next.next = new ListNode(3);
+// const l2 = new ListNode(5);
+// l2.next = new ListNode(6);
+// l2.next.next = new ListNode(4);
+// console.log(addTwoNumbers(l1, l2));
+
+//无重复字串
+function lengthOflongestSubstring(str) {
+  const len = str.length;
+  if (len === 0) return 0;
+  let maxLength = 0;
+  let j = 0;
+  let set = new Set();
+  for (let i = 0; i < len; i++) {
+    if (!set.has(str[i])) {
+      set.add(str[i]);
+      maxLength = Math.max(maxLength, set.size);
+    } else {
+      while (set.has(str[i])) {
+        set.delete(str[j]);
+        j++;
       }
+      set.add(str[i]);
     }
   }
-  // 若目标总额对应的解为无穷大，则意味着没有一个符合条件的硬币总数来更新它，本题无解，返回-1
-  if (f[amount] === Infinity) {
-    return -1;
-  }
-  // 若有解，直接返回解的内容
-  return f[amount];
-};
-
-// const coins = [1, 2, 5],
-//   mount = 11;
-
-// console.log(coinsChange(coins, mount));
-//不同路径
-function difPaths(m, n) {
-  let res = Array(m).fill(Array(n).fill(1));
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      res[i][j] = res[i][j - 1] + res[i - 1][j];
-    }
-  }
-  return res[m - 1][n - 1];
+  return maxLength;
 }
-// const m=7,n=3;
-// console.log(difPaths(m,n))
-
-//买卖股票的最佳时机
-function maxProfit(prices) {
-  let profit = 0;
-  for (let i = 0; i < prices.length - 1; i++) {
-    if (prices[i + 1] > prices[i]) {
-      profit += prices[i + 1] - prices[i];
+// console.log(lengthOflongestSubstring("abcbcbc"));
+//最长回文子串
+function longestPil(s) {
+  if (s.length <= 0) return s;
+  let maxLength = 1;
+  let start = 0;
+  const len = s.length;
+  function h(left, right) {
+    while (left >= 0 && right < len && s[left] === s[right]) {
+      if (right - left + 1 > maxLength) {
+        maxLength = right - left + 1;
+        start = left;
+      }
+      left--;
+      right++;
     }
   }
-  return profit;
-}
-// const prices = [7,6,4,7,8]
-// console.log(maxProfit(prices));
-
-function pubFront(strs) {
-  if (Object.prototype.toString.call(strs) !== "[object Array]")
-    throw new TypeError("this is not an array");
-  let str = strs.length > 0 ? strs[0] : "";
-  for (let key in strs) {
-    if (str === "") break;
-    const item = strs[key];
-    while (!item.includes(str)) {
-      if (str === "") break;
-      str = str.substring(0, str.length - 1);
-    }
+  for (let i = 1; i < len; i++) {
+    h(i - 1, i + 1);
+    h(i, i + 1);
   }
-  return str;
+  return s.substring(start,start+maxLength)
 }
-// const strs = ["dog","racecar","car"]
-// console.log(pubFront(strs));
+console.log(longestPil('abcbdbd'))
 
-//冒泡排序，层层遍历若后面大于前面则交换相邻
+//冒泡排序，层层历若后面大于前面则交换相邻
 function bubbleSort(arr) {
   let len = arr.length;
   for (let i = 0; i < len; i++) {
@@ -342,8 +389,8 @@ function partition(arr, l, r) {
   swap(arr, j, l);
   return j;
 }
-const arr = [2, 1, 4, 3, 3, 2, 7, 9, 4, 32, 1];
-console.log(quickSort(arr));
+// const arr = [2, 1, 4, 3, 3, 2, 7, 9, 4, 32, 1];
+// console.log(quickSort(arr));
 //归并排序
 function mergeSort(arr) {
   let len = arr.length;
